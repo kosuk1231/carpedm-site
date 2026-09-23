d7bb82917ba44271f8313dbeee08e174e8c0d997
kosuk1231 <kosuk1231@gmail.com>
2026-09-23 14:40:54 +0900

    fix: bundle instructor photo as static image import

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import profilePhoto from '../public/profile/kosukwoo.jpg';
 
 const WORK = [
   {
@@ -765,13 +766,12 @@ export default function Home() {
             <div className="speaker-id">
               <div className="speaker-photo">
                 <Image
-                  src="/profile/kosukwoo.jpg"
+                  src={profilePhoto}
                   alt="고석우 강사 프로필 사진"
                   width={440}
                   height={685}
                   sizes="(max-width: 680px) 84px, 112px"
                   priority
-                  unoptimized
                 />
               </div>
               <div>
